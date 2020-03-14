1d8cfd869bc7f5cd4b673cb5c06951e220620bc6
Niladree Datta <44455426+NiladreeDatta@users.noreply.github.com>
2020-03-14 05:37:41 -1200

    Add files via upload

---
+++ 1325A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tc,x;
+    cin >> tc;
+    while(tc--)
+    {
+        cin >> x;
+        cout << "1" << " " << x-1 << endl;
+    }
+
+    return 0;
+}
