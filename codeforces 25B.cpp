37e1335279e8d310dd0e102a2761f80d3662b2eb
Niladree Datta <44455426+NiladreeDatta@users.noreply.github.com>
2020-04-08 10:13:47 -1200

    Add files via upload

---
+++ codeforces 25B.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    string s;
+    cin >> n >> s;
+    for(int i = 0 ; i < n ; i++)
+    {
+        cout << s[i];
+        if(i%2 == 1 && n-i > 2)
+            cout << "-";
+    }
+
+    return 0;
+}
