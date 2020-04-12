84f504bde5edb9654906e6b9840cb14ccd2d2068
Niladree Datta <44455426+NiladreeDatta@users.noreply.github.com>
2020-04-12 05:51:03 -1200

    Add files via upload

---
+++ codefroces 1338 A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int tc,n;
+    cin >> tc;
+    while(tc--)
+    {
+        cin >> n;
+        cout << n << endl;
+    }
+
+    return 0;
+}
