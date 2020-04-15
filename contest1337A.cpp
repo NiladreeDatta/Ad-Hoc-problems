dcae4b584a5b700676157c50d7f826e19e7a55d3
Niladree Datta <44455426+NiladreeDatta@users.noreply.github.com>
2020-04-15 07:20:58 -1200

    Add files via upload

---
+++ contest1337A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int a,b,c,d,tc;
+    cin >> tc;
+    while(tc--)
+    {
+        cin >> a >> b >> c >> d;
+        cout << b << " " << c << " " << c << endl;
+
+    }
+
+    return 0;
+}
