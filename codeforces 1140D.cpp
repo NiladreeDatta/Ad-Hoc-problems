27525c0e066d8ab9367a3a477566b754e452a3f7
Niladree Datta <44455426+NiladreeDatta@users.noreply.github.com>
2020-04-29 15:09:29 +0600

    Add files via upload

---
+++ codeforces 1140D.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n ;
+    long long ans = 0 ;
+    for(int i = 2 ; i < n ; i++)
+    {
+        ans += 1ll * i * (i+1);
+    }
+    cout << ans <<endl;
+
+    return 0;
+}
